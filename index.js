69e3dc81cc863b5c2f4bcf3ac74380c1e75452c9
christ0ph3r <gitgitgit@protonmail.com>
2017-10-25 15:34:25 +0700

    First commit :)

---
+++ index.js
@@ -0,0 +1,124 @@
+#! /usr/bin/env node
+
+const each = require('foreach');
+const request = require('request');
+const rp = require('request-promise');
+const chalk = require('chalk');
+const Table = require('cli-table');
+const jsonfile = require('jsonfile');
+const figlet = require('figlet');
+const file = './config.json';
+const apiKey = jsonfile.readFileSync(file);
+const currSym = '$';
+
+figletLog('Checking your uptime...');
+
+var options = {
+  method: 'POST',
+  uri: 'https://api.uptimerobot.com/v2/getMonitors',
+  body: {
+    api_key: apiKey.key,
+    all_time_uptime_ratio: 1,
+    response_times_average: 1,
+  },
+  json: true // Automatically stringifies the body to JSON
+};
+
+rp(options)
+.then(function (parsedBody) {
+   var table = new Table({ head: [
+    chalk.blue('ID'),
+    chalk.blue('Name'),
+    chalk.blue('URL'),
+    chalk.blue('Status'),
+    chalk.blue('Type'),
+    chalk.blue('Interval'),
+    chalk.blue('Uptime'),
+    chalk.blue('Created'),
+  ] });
+  each(parsedBody.monitors, function (value, key, array) {
+    table.push([
+      chalk.blue(value.id),
+      chalk.green(value.friendly_name),
+      chalk.green(value.url),
+      chalk.green(monitorStatus(value.status)),
+      chalk.green(monitorType(value.type)),
+      chalk.green(value.interval),
+      chalk.green(value.all_time_uptime_ratio+'%'),
+      chalk.green(new Date(value.create_datetime * 1000).toUTCString()),
+    ]);
+  });
+  console.log('\n'+table.toString());
+})
+.catch(function (err) {
+  console.log('Something went wrong...');
+  console.log(err);
+});
+
+/**
+ * Figlet console log
+ */
+
+function figletLog(text) {
+  figlet(text, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data)
+  });
+}
+
+/**
+ * Switch case for monitor types
+ * Makes API data understandable
+ */
+
+function monitorType(typeNumber) {
+  switch(typeNumber) {
+    case 1:
+      type = "HTTP(s)";
+      break;
+    case 2:
+      type = "Keyword";
+      break;
+    case 3:
+      type = "Ping";
+      break;
+    case 4:
+      type = "Port";
+      break;
+    default:
+      type = "Unknown";
+  }
+  return type;
+}
+
+/**
+ * Switch case for monitor statuses
+ * Makes API data understandable
+ */
+
+function monitorStatus(statusNumber) {
+  switch(statusNumber) {
+    case 0:
+      status = "Paused";
+      break;
+    case 1:
+      status = "Not Checked Yet";
+      break;
+    case 2:
+      status = "Up";
+      break;
+    case 8:
+      status = "Seems Down";
+      break;
+    case 9:
+      status = "Down";
+      break;
+    default:
+      status = "Unknown";
+  }
+  return status;
+}
---
+++ readme.md
@@ -0,0 +1,66 @@
+# Uptime CLI
+
+Uptime CLI lets you monitor websites and track uptime/downtime through the command line.  It uses the uptimerobot.com API to fetch data. Uptimerobot's free option allows you to monitor up to 50 websites free.
+
+![Uptime CLI](https://i.imgur.com/EUjtIhN.png)
+
+## Features
+
+### General
+1. Command Line Interface
+1. Track Websites Uptime/Downtime
+
+### Monitor Data Table
+1. ID
+1. Name
+1. URL
+1. Status
+1. Type
+1. Interval
+1. Uptime %
+1. Date Created
+
+## Installation Instructions
+
+
+1. Git Clone the repo
+
+    ```
+    git clone https://github.com/christ0ph3r/uptime-cli/
+    ```
+
+1. Enter the repository and copy the config file
+
+    ```
+    cd uptime-cli && cp config-sample.json config.json
+    ```
+
+1. Install NPM packages
+
+    ```
+    npm install
+    ```
+
+1. Edit config.json
+
+    ```json
+    {
+      "key": "xxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+    }
+    ```
+
+1. Run
+
+  ```
+  npm start
+  ```
+
+## Want to support my work? Donations accepted
+
+
+| Coin     | Address                                    |
+| -------- |:------------------------------------------:|
+| Bitcoin  | 1LFTccjYHbiVekdm8XYC1ucNqdGsAC3frc         |
+| Ethereum | 0x071Fe2Bb50430A3f6af398A410a78B67e1A783AE |
+| Litecoin | Lh9eV96yhTyrkv2VkWG7RZvas9TzFuYZbR         |
+
